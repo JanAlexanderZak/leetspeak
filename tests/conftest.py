8fbc2ff5584d1d79e77522f27c35823b8c2237ce
JanAlexanderZak <janalexzak@gmail.com>
2020-11-24 10:50:21 +0100

    add: tests

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from src.leetspeak import LeetspeekConverter
+
+
+@pytest.fixture
+def my_fixture():
+    return 7
+
+
+@pytest.fixture
+def instantiate_converter():
+    def _method(string):
+        """Kinda wrap to pass in parameter"""
+        converter = LeetspeekConverter()
+        leet_string = converter.convert_to_leet(string)
+        return leet_string
+    return _method
--- tests/src/test_leetspeak.py
+++ tests/src/test_leetspeak.py
@@ -1,7 +1,26 @@
-from src.main import NeuralNetwork
+import pytest
 
 
-def test_get_attr():
-    neural_network = NeuralNetwork(1, 2, 3, 4)
-    attribute = neural_network.get_attr()
-    assert attribute == 1
+
+
+def test_fixture(my_fixture):
+    assert my_fixture == 7
+
+
+def test_convert_to_leet(instantiate_converter):
+    leet_string = instantiate_converter("test string")
+    assert isinstance(leet_string, str)
+
+
+@pytest.mark.parametrize(
+    'string, expected', [
+        ("alex", "4l3x"),
+        ("cool", "c00l"),
+        ("love", "l0v3"),
+        ("leet", "l337"),
+        ("special charackters", "5p3c14l ch4r4ck73r5"),
+    ]
+)
+def test_sample_strings(string, expected, instantiate_converter):
+    leet_string = instantiate_converter(string)
+    assert leet_string == expected
